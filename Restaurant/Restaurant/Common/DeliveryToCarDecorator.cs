8459a4d243929e287f8811a813708253a11ed287
Michiel <michieljansen1@live.nl>
2020-04-03 16:06:49 +0200

    add: decorator call including parkinglot A and B and table inside and outside

---
+++ Restaurant/Restaurant/Common/DeliveryToCarDecorator.cs
@@ -0,0 +1,33 @@
+﻿using Restaurant.Decorators;
+using Restaurant.Models;
+
+namespace Restaurant.Common
+{
+    class DeliveryToCarDecorator : DeliveryDecorator
+    {
+        public DeliveryToCarDecorator(Delivery delivery) : base(delivery)
+        {
+
+        }
+        public override string DeliveryType(string deliveryType)
+        {
+            if(deliveryType == "ToCarA")
+            {
+                return "The food will be send to your car on parking lot A";
+            }
+            else if (deliveryType == "ToCarB")
+            {
+                return "The food will be send to your car on parking lot B.";
+            }
+            else
+            {
+                return this.GetOrder().DeliveryType(deliveryType);
+            }
+        }
+
+        public override int GetDiscount()
+        {
+            return 5;
+        }
+    }
+}
