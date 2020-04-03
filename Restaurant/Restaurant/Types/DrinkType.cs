36c3d0207ac59bdbd11ee16ff8af2a2e1617173d
Yaron <yaronlambers@gmail.com>
2020-04-03 09:50:23 +0200

    Added default types for menu, burger,  size, wrap, burger

---
+++ Restaurant/Restaurant/Types/DrinkType.cs
@@ -0,0 +1,16 @@
+﻿using Restaurant.Attributes;
+
+namespace Restaurant.Types
+{
+    public enum DrinkType
+    {
+        [MenuPriceAttribute(1.50)]
+        Sinas,
+
+        [MenuPriceAttribute(1.50)]
+        Cola,
+
+        [MenuPriceAttribute(1.50)]
+        ColaZero
+    }
+}
---
+++ Restaurant/Restaurant/Types/SideType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Restaurant.Attributes;
+
+namespace Restaurant.Types
+{
+    public enum SideType
+    {
+        [MenuPrice(1.50)]
+        Fries,
+
+        [MenuPriceAttribute(0.75)]
+        Salad
+    }
+}
