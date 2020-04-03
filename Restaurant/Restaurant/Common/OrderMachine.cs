00938e2f2bcfeedf14ba3cedb384dd62af3d62a6
Thomas Christerus <tchristerus@outlook.com>
2020-04-03 14:20:25 +0200

    Add: Ordermachine core logic

--- Restaurant/Restaurant/Common/OrderMachine.cs
+++ Restaurant/Restaurant/Common/OrderMachine.cs
@@ -1,28 +1,53 @@
-﻿using Restaurant.Memento;
+﻿using Restaurant.Factories;
+using Restaurant.Memento;
 using Restaurant.Models;
 using Restaurant.Types;
 
 namespace Restaurant.Common
 {
     /// <summary>
-    /// 
+    ///     Main core logic
     /// </summary>
     class OrderMachine
     {
-        private ProfileCaretaker _profileCaretaker;
-        private Profile _activeProfile;
+        private Profile _activeProfile; // current profile that is ordering
+        private ProfileCaretaker _profileCaretaker; // keeps track of the memento's
 
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        /// <param name="orderType"></param>
         public OrderMachine(string orderType = null)
         {
-            
+            NewProfile();
         }
 
-        public void AddProductToProfile(DrinkType drinkType, SideType sideType, MainDishType mainDishType, SizeType sizeType)
+        /// <summary>
+        ///     Creates a new menu and adds it to the current active profile
+        /// </summary>
+        /// <param name="menuType">MenuType to create</param>
+        /// <param name="drinkType">Type of drink to add</param>
+        /// <param name="sideType">Type of menu side to add</param>
+        /// <param name="mainDishType">Type of the main dish</param>
+        /// <param name="sizeType">Size of the dish</param>
+        public void AddProductToProfile(MenuType menuType, DrinkType drinkType, SideType sideType,
+            MainDishType mainDishType, SizeType sizeType)
         {
+            var menu = new DishFactory(
+                    menuType,
+                    drinkType,
+                    sideType,
+                    mainDishType,
+                    sizeType)
+                .CreateMenu();
 
-            // _activeProfile.AddItem();
+            _activeProfile.AddItem(menu);
         }
 
+        /// <summary>
+        ///     Switches to the requested profile
+        /// </summary>
+        /// <param name="Id">Id of the profile to switch to</param>
         public void SwitchProfile(int Id)
         {
             var profileMemento = _activeProfile.MakeMemento();
@@ -32,14 +57,22 @@ namespace Restaurant.Common
             _activeProfile.LoadMemento(newProfile);
         }
 
+        /// <summary>
+        ///     Creates a new profile and sets it as active
+        /// </summary>
         public void NewProfile()
         {
             _activeProfile = new Profile(_profileCaretaker.TotalProfiles);
         }
 
+        /// <summary>
+        ///     Finalizes the current order
+        /// </summary>
         public void Finish()
         {
+            // ....
 
+            _profileCaretaker.Clear();
         }
     }
 }
